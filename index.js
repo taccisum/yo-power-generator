2b72ed4e0c48dd6ea708af9356e599790e5abf6e
taccisum <liaojinfeng@deepexi.com>
2019-07-02 15:14:45 +0800

    feat: support sub prompt

--- index.js
+++ index.js
@@ -3,22 +3,21 @@ var Generator = require('yeoman-generator');
 
 const path = require('path');
 const fileUtils = require('./lib/util/file_utils');
-const Adapter = require('./lib/adapter');
+const Argument = require('./lib/argument');
 const Factory = require('./lib/factory');
 const debug = require('debug')('pg:generator');
 
 module.exports.AbstractTemplateHandler = require('./lib/abstract_template_handler');
 module.exports.FileUtils = fileUtils;
 module.exports.getGenerator = (args0, opt) => {
-  args0 = new Adapter(args0);
-
   return class extends Generator {
     constructor (args, opts) {
       super(args, opts);
+      this.argument = Argument.builder(args0, this);
       const _this = this;
 
       this.option('command', { desc: '使用命令模式（非交互操作）', alias: 'c', type: Boolean, default: false });
-      args0.toOptions().forEach(option => {
+      this.argument.options().forEach(option => {
         _this.option(option.key, option.val);
       })
       this.factory = new Factory(opt.handlerDir);
@@ -32,7 +31,7 @@ module.exports.getGenerator = (args0, opt) => {
 
     async prompting () {
       if (!this.options.command) {
-        const answer = await this.prompt(args0.toPromptings());
+        const answer = await this.argument.prompt();
 
         const cliOptsStr = Object.keys(answer).map(key => {
           if (answer[key]) {
@@ -47,7 +46,7 @@ module.exports.getGenerator = (args0, opt) => {
       } else {
         const _this = this;
         this.props = {};
-        args0.toOptions().forEach(option => {
+        this.argument.options().forEach(option => {
           this.props[option.key] = _this.options[option.key];
         })
       }

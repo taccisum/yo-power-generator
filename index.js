192db64c2a4e0776ec0542c5aaa6f76e6fbb464a
LinYuanTongXue <ab765371578@qq.com>
2019-09-11 14:27:06 +0800

    feat: 添加选项条件触发器支持与 --f --desc 表单与描述选项

--- index.js
+++ index.js
@@ -1,48 +1,50 @@
 'use strict'
 var Generator = require('yeoman-generator');
 
+const _ = require('lodash');
 const path = require('path');
 const fileUtils = require('./lib/util/file_utils');
 const Argument = require('./lib/argument');
 const Factory = require('./lib/factory');
 const debug = require('debug')('pg:generator');
+const process = require('process');
 
+module.exports.Trigger = {
+  AnswerTrigger: require('./lib/trigger/answer_trigger')
+};
 module.exports.AbstractTemplateHandler = require('./lib/abstract_template_handler');
 module.exports.FileUtils = fileUtils;
-module.exports.getGenerator = (args0, opt) => {
+module.exports.getGenerator = (info, options, opt) => {
   return class extends Generator {
     constructor (args, opts) {
       super(args, opts);
-      this.argument = Argument.builder(args0, this);
+      this.argument = Argument.builder(options, this);
       const _this = this;
 
       this.option('command', { desc: '使用命令模式（非交互操作）', alias: 'c', type: Boolean, default: false });
+      this.option('description', { desc: '脚手架描述', alias: 'desc', type: Boolean, default: false });
+      this.option('form', { desc: '脚手架表单', alias: 'f', type: Boolean, default: false });
+
       this.argument.options().forEach(option => {
         _this.option(option.key, option.val);
       })
       this.factory = new Factory(opt.handlerDir);
     }
 
-    catch (e) {
-      // if (e) {
-      // console.log(e)
-      // }
-    };
-
     async prompting () {
+      if (this.options.description) {
+        this.log(info.description);
+        process.exit();
+      }
+
+      if (this.options.form) {
+        const form = this._toForm(options);
+        this.log(JSON.stringify(form));
+        process.exit();
+      }
+
       let cliOptsStr = '';
-      if (!this.options.command) {
-        const answer = await this.argument.prompt();
-
-        cliOptsStr = Object.keys(answer).map(key => {
-          if (answer[key]) {
-            return `--${key}=${answer[key]}`;
-          }
-        }).filter(val => { return !!val }).join(' ');
-
-        this.props = answer;
-        this.props.mode = 'interaction';
-      } else {
+      if (this.options.command) {
         const _this = this;
         this.props = {};
 
@@ -53,6 +55,17 @@ module.exports.getGenerator = (args0, opt) => {
           this.props[key] = val;
         })
         this.props.mode = 'command';
+      } else {
+        const answer = await this.argument.prompt();
+
+        cliOptsStr = Object.keys(answer).map(key => {
+          if (answer[key]) {
+            return `--${key}=${answer[key]}`;
+          }
+        }).filter(val => { return !!val }).join(' ');
+
+        this.props = answer;
+        this.props.mode = 'interaction';
       }
       this.props.cli = cliOptsStr.trim();
 
@@ -72,6 +85,33 @@ module.exports.getGenerator = (args0, opt) => {
           debug(`file ${f} is not a template.`)
         }
       })
+    }
+
+    _toForm (opt) {
+      const form = {};
+      Object.keys(opt).forEach(key => {
+        const val = opt[key];
+        if (_.isEmpty(val)) {
+          return;
+        }
+        form[key] = val.prompting;
+        if (val.option) {
+          const defaultVal = val.option.default;
+          if (!_.isEmpty(defaultVal)) {
+            form[key].default = defaultVal;
+          }
+        }
+        if (val.callbacks && _.isArray(val.callbacks.trigger)) {
+          form[key].trigger = [];
+          for (const trigger of val.callbacks.trigger) {
+            form[key].trigger.push(trigger.toForm());
+          }
+        }
+        if (val.child) {
+          form[key].child = this._toForm(val.child);
+        }
+      });
+      return form;
     }
   }
 }
